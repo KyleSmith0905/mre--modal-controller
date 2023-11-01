196aba187120f3afb42ddf9c7f37ad75549c25e0
kylesmith090502 <me@yskkyle.com>
2023-11-01 13:38:44 -0400

    Initial commit

---
+++ capacitor.config.ts
@@ -0,0 +1,12 @@
+import { CapacitorConfig } from '@capacitor/cli';
+
+const config: CapacitorConfig = {
+  appId: 'io.ionic.starter',
+  appName: 'mre--modal-controller',
+  webDir: 'www',
+  server: {
+    androidScheme: 'https'
+  }
+};
+
+export default config;
